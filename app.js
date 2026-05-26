b1ec482543e19ed6e0b22b9070a2042a442f99b4
BCS259 <businesscoresolutions.com>
2026-05-26 09:44:35 +0530

    [Code]

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-// Reverse String
-const name="Abarna";
-const array=[];
-let reverseString=""
-for(i=name.length-1;i>=0;i--){
-    array.push(name[i]);
-    reverseString=`${reverseString}${name[i]}`
+//1 . Reverse String
+
+const reverseString = (name) => {
+    const array = [];
+    let reverseString = ""
+    for (i = name.length - 1; i >= 0; i--) {
+        array.push(name[i]);
+        reverseString = `${reverseString}${name[i]}`
+    }
+    const reverseArray = array.join("");
+    return reverseArray
 }
-const reverseArray=array.join("");
-console.log(name,reverseArray,reverseString)
+
+console.log("Reverse String", reverseString("Abarna"));
+
+// 2 . Check Palindrome
+
+const checkPalindrome = (number) => {
+    const reverseNumber = reverseString(`${number}`);
+    return String(number) === reverseNumber;
+}
+
+console.log("Check Palindrome", checkPalindrome(1221));
+
+
+// 3 .Swap two numbers
+
+
+const swapTwoNumbers = (firstNum, secondNum) => {
+    const temp = firstNum;
+    const first = secondNum;
+    const second = temp;
+    return `${first} ${second}`
+}
+
+console.log("Swap two Number", swapTwoNumbers(20, 30))
